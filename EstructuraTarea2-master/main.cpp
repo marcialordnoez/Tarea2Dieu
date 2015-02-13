36bf41a07fbb9384fd564574e50e7e4db1d7b466
marcialordnoez <marcial.ordonez@unitec.edu>
2015-02-12 18:31:20 -0600

    Paz sobre todo entendimiento

--- EstructuraTarea2-master/main.cpp
+++ EstructuraTarea2-master/main.cpp
@@ -12,15 +12,18 @@ using namespace std;
 //devuelve true si todos los elementos de la cola son pares, de lo contrario devuelve false
 bool sonPares(queue<int> mi_cola)
 {
-    while(!mi_cola.empty())///verifico si esta vacia mi cola
-        {
-            if(mi_cola.front()%= 2)///divido toda mi cola
-                {
-                 return false;///que retorne falso
-
-                }mi_cola.pop();///que saque elementos
-        }
-    return true;/// que retorne verdadero
+   while(!mi_cola.empty())//vinedo si mi cola tiene elementos
+{
+if(mi_cola.front()%2 == 0)//examinando cada elemento y viendo si tiene multiplos de 2
+{
+mi_cola.pop();//sacando el primer elemento para revisar toda mi cola
+}
+else
+{
+return false;
+}
+}
+return true;
 }
 
 //devuelve true si todos los elementos de la lista son pares, de lo contrario devuelve false
@@ -33,117 +36,91 @@ bool sonPares(list<int> mi_lista)
 //                    return false;
 //                }mi_lista.pop_front();
 //        }
-       list<int>::iterator it;///declarando iterator
-
-    for(it=mi_lista.begin(); it!=mi_lista.end(); it++)///recorriendo la lista con iterator
+        while(!mi_lista.empty())//revisar si mi lista tiene elementos
+{
+        if(mi_lista.front()%2 == 0)//ver que cada elemento de mi lista tiene multiplo de 2
     {
-        if(*it %= 2)///dividiendo toda mi lista
+    mi_lista.pop_front();//voy sacando el primer elemento de mi lista
+    }       else
             {
-                return false;///que retorne falso
-            }mi_lista.pop_front();///que saque el primer elemento que esta enfrente
-    }
-
-    return true;///que retorne verdadero
+                return false;
+            }
+}
+    return true;
 }
 
 //devuelve true si str es un elemento de mi_cola, de lo contrario devuelve false
 bool existe(queue<string> mi_cola, string str)
 {
-    while(!mi_cola.empty())///verifico si esta vacia mi cola
-        {
-            if(mi_cola.front()==str)///igualando el primer elemento de mi cola con str
-                {
-                    return true;///que retorno verdader
-                }mi_cola.pop();///que saque los elementos
-        }
-    return false;///que retorne falso
+    while(!mi_cola.empty())//revisando si mi cola tiene elementos
+{
+if(mi_cola.front() == str)//igualando mi cola igual a str para ver si se encuentra
+{
+return true;
+}
+mi_cola.pop();//sacando elementos de mi colalo hago fuera del if por que si encuentra el elemento de primero que de ahi de un solo se salga
+}
+return false;
 }
 
 //devuelve true si str es un elemento de mi_cola, de lo contrario devuelve false
 bool existe(list<string> mi_lista, string str)
+{ while(!mi_lista.empty())//revisando los elementos de mi lista
 {
-//    while(!mi_lista.empty())
-//        {
-//            if(mi_lista.front()==str)
-//                {
-//                    return true;
-//                }mi_lista.pop_front();
-//        }
-           list<string>::iterator it;///declarando iterator
-
-    for(it=mi_lista.begin(); it!=mi_lista.end(); it++)///recorriendo la lista usando iterator
-    {
-            if(*it == str)///igualando el iterator con str
-                {
-                    return true;///que retorne true
-                }
-    }
-
-    return false;///que retorne false
+if(mi_lista.front()==str)//lo igualo ala variable para ver si lo encuentra
+{
+return true;
+}
+mi_lista.pop_front();//saco el elemento
+}
+return false;
 }
 
 //devuelve la suma de los elementos de la cola
 int getSuma(queue<int> mi_cola)
 {
-    int cont = 0;///decalrando variables
-
-    while(!mi_cola.empty())///verificando si esta vacia
-        {
-            cont += mi_cola.front();/// que se guarde lo que esta en el comienzo de mi cola en cont y que lo sume
-            mi_cola.pop();///sancando elementos de mi cola
-        }
-    return cont;///que retorne cont
+   int suma = 0;//declaro mi variable
+while(!mi_cola.empty())//verifico si hay elementos en mi cola
+{
+suma += mi_cola.front();//hago que todo los elementos de mi cola se sumen y se guarden en mi variable
+mi_cola.pop();//voy sacando cada elemento
+}
+return suma;
 }
 
 //devuelve la suma de la cantidad letras de cada cadena de la lista
 int sumarLetras(list<string> mi_lista)
 {
-    int suma = 0;///declarando variables
+    int suma = 0;///declarando variables y lo igualo a cero
     string dato;///declarando variables
-    list<string>::iterator it;///declarando iterator
+    list<string>::iterator it;///declarando iterator tipo string
 
-    for(it=mi_lista.begin(); it!=mi_lista.end(); it++)///recorriendo mi lista
+    for(it=mi_lista.begin(); it!=mi_lista.end(); it++)///recorriendo mi lista desde el principio y el final
     {
-            dato = *it;///guardo el valor de mi iterator en dato
-            suma += dato.length();///busco el tamaño de mi cadena y luego lo sumo y guardo el valor en suma
+            dato = *it;///dato guarda lo que tiene mi iterator
+            suma += dato.length();///la suma va sumando la cadena que viene gurdado mi variable dato
     }
 
-     return suma;/// que retorne suma
+     return suma;/// que retorne suma de todas las cadenas
 }
 
 //Devuelve true si los elementos de la lista son estan ordenados alfabeticamente, de lo contrario devuelve false
 bool estaOrdenada(list<char>mi_lista)
 {
-//    list<char>hola = mi_lista;
-//    hola.sort();
-
-//    while(!mi_lista.empty())
-//        {
-//            if(mi_lista.front()==hola.front())
-//                {
-//                    mi_lista.pop_front();
-//                    hola.pop_front();
-//                }else
-//                {
-//                    return false;
-//                }
-//        }
-
-    list<char>aux =  mi_lista;///decalro una lista auxiliar y la igualo a mi lista
-    aux.sort();///mi mista auxiliar la vuelvo ordenada con la funcion sort
-
-    for(list<char>::iterator it= mi_lista.begin(); it!= mi_lista.end(); it++)///recorro la lista
-    {
-            if(mi_lista.front() == aux.front())///igualo los primeros elementos de las dos listas
-            {
-                mi_lista.pop_front();///saco los elementos
-                aux.pop_front();///saco el elemento
-            }else
-            {
-                return false;///retorna falso
-            }
-    }
-    return true;///retorna verdadero
+ list<char>auxiliar = mi_lista;//declaro una nueva lista y la igualo a mi lista
+auxiliar.sort();//declaro la funcion sort para ordenar mi lista declarada
+while(!mi_lista.empty())//viendo si hay elemento en mi lista
+{
+if(mi_lista.front()==auxiliar.front())//igualando el primer elemento de cada lista
+{ //para verificar si cada elemento de mi lista es igual a cada elemento de mi auxiliar
+mi_lista.pop_front();//sacando el primer elemento
+auxiliar.pop_front();//sacando el primer elemento
+}else
+{
+return false;
+}
+}
+return true;
 }
 
 int main ()
